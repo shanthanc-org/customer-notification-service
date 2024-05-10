645aa0f089e503905410000d5d0b1d22df259ab3
shanthanc <shanthanc@gmail.com>
2024-05-10 11:42:34 -0500

    First commit

---
+++ src/main/java/org/shanthan/customernotificationservice/CustomerNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package org.shanthan.customernotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerNotificationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/shanthan/customernotificationservice/CustomerNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shanthan.customernotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerNotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
